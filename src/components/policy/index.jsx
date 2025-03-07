329647494676f9b621da77b88d3d2aeab82adca0
Lindimphilo WJ <66278795+Libertalia-estate-agency@users.noreply.github.com>
2025-03-07 22:35:21 +0200

    privacy policy

--- src/components/policy/index.jsx
+++ src/components/policy/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { FooterFive, Header, Wrapper } from '../../layout';
 import { animationCreate } from '../../utils/utils';
 import Breadcrumb from '../common/breadcrumb/breadcrumb';
-import Cta from '../common/cta/cta';
+//import Cta from '../common/cta/cta';
 import PolicyArea from './policy-area';
 
 
@@ -18,10 +18,11 @@ const PolicyMain = () => {
     <Wrapper>
       <Header />
       <Breadcrumb title={'Privacy Policy'} />
-      
+      <PolicyArea />
+
       <FooterFive />
     </Wrapper>
   );
 };
 
-export default Faq;
+export default PolicyMain;
--- src/components/policy/policy-area.jsx
+++ src/components/policy/policy-area.jsx
@@ -7,7 +7,7 @@ const PolicyArea = () => {
         <div className="row">
           <div className="col-12">
             <div className="cd-info-box">
-              <h3 className="tp-title-sm pb-30">Introducing Shine: the way to live mindfully</h3>
+              <h3 className="tp-title-sm pb-30">Privacy Policy</h3>
               <p className="mb-25">
               This Privacy Policy describes our policies and procedures on the collection, use and disclosure of your information when you use our site and tells you about your privacy rights.
 
@@ -15,14 +15,14 @@ const PolicyArea = () => {
 
                 Personal Data
 
-                While using our site, we may ask you to provide certain personally identifiable information that can be used to contact or identify you. Personally identifiable information includes data like:
+                While using our site, we may ask you to provide certain personally identifiable information that can be used to contact or identify you. Personally identifiable information includes data like: <br />
 
-                Email address
-                First name and last name
-                Phone number
+                Email address <br />
+                First name and last name <br />
+                Phone number  <br />
                 Address, Province, Postal code, Suburb
-                Usage Data
-                Usage Data
+                <br /><br />
+                <strong>Usage Data </strong><br />
 
                 Usage Data is collected automatically when using our site.
 
@@ -32,7 +32,7 @@ const PolicyArea = () => {
 
                 We may also collect information that your browser sends whenever you visit our site or when you access the site by or through a mobile device.
               </p>
-              <p className="mb-25">Tracking Technologies and Cookies
+              <p className="mb-25"><strong>Tracking Technologies and Cookies </strong><br />
 
                     We use Cookies and similar tracking technologies to track the activity on our site and store certain information. A cookie is a small file placed on your device. Unless you have adjusted Your browser setting so that it will refuse Cookies, our site may use Cookies.
 
@@ -42,27 +42,30 @@ const PolicyArea = () => {
                     To contact you by email, telephone, SMS, WhatsApp or other equivalent forms of electronic communication regarding updates or informative communications related to real estate services,
                     To provide you with news and general information about real estate related topics unless you decide not to receive such information and Opt-Out.
                     We may use your information for other purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our site, services, marketing and your experience.
-                    Legal requirements
+                    <br /> <br />
+                    <strong>Legal requirements</strong><br />
 
                     Under certain circumstances, the company may be required to disclose your Personal Data if required to do so by law or in response to valid requests by public authorities (e.g. a court or a law enforcement).
 
-                    Security of Your Personal Data
+                    <br /> <br />
+                    <strong>Security of Your Personal Data</strong> <br />
 
                 The security of Your Personal Data is important to us that is why we make use of industry leading security- and hosting solutions to protect your Personal Data.</p>
-              <p>Links to Other Websites
+              <p><strong>Links to Other Websites</strong> <br />
 
                 Our Service may contain links to other websites that are not operated by us. If You click on a third-party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.
 
                 We have no control over and assume no responsibility for the content, privacy policies or practices of any third-party sites or services.
-
-                Changes to this Privacy Policy
-
+                <br /> <br />
+                <strong> Changes to this Privacy Policy</strong>
+                <br />
                 We may update Our Privacy Policy from time to time which will be reflected in the "Last updated" date at the bottom of this Privacy Policy.
 
                 You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page. Your continued use of the site after we post any modifications will be seen as your acknowledgment and your consent to abide by and be bound by the modified Privacy Policy.
-
-                Last Updated: 1 January 2022
-
+                <br /> <br />
+                <strong>Last Updated: 1 January 2022</strong>
+                <br /> <br />
+                
                 If you have any questions about this Privacy Policy, please contact us.</p>
             </div>
           </div>
@@ -72,4 +75,4 @@ const PolicyArea = () => {
   );
 };
 
-export default IntroduceArea;
+export default PolicyArea;
